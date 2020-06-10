755afbb7a1aec5176988cc5f2c3abffc873fb5ce
Egor Siropov <stafffsss@mail.ru>
2020-06-10 16:30:19 +0300

    norm. parsing

--- graduation_project_VK.py
+++ graduation_project_VK.py
@@ -1,12 +1,13 @@
 import requests
 import time
+import json
 
 token = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
 url_gloups = 'https://api.vk.com/method/groups.get'
 url_friends = 'https://api.vk.com/method/friends.get'
 url_groups_count_people = 'https://api.vk.com/method/groups.getMembers'
+url_not_user_in_group = "https://api.vk.com/method/groups.isMember"
 
-account_succses = 0
 class User():
     """Определение групп пользователя. Парсинг друзей пользователя. Поиск групп, в которых остоит только User"""
 
@@ -29,25 +30,16 @@ class User():
         try:
             items = group_file.json()['response']['items']
             self.list_group = []
-
             for el in items:
                 personal_group = el['id'], el['name']
                 self.list_group.append(list(personal_group))
-
-            global account_succses
-            account_succses += 1
-            self.count_people_in_group()
-            # print(f'Анализ групп у vk.com/id{self.us_id}')
             return self.list_group
-
         except:
             print(f'Нет групп или ограничен доступ к vk.com/id{self.us_id}\nИдет итерация...' )
 
-    def count_people_in_group(self):
+
+    def count_people_in_group(self, id_group):
         # № 4 Количество пользователей в группе
-
-        count_people_in_group = []
-
         for group in self.list_group:
             time.sleep(0.4)
             id_group = group[0]
@@ -58,30 +50,64 @@ class User():
             try:
                 group_file = requests.get(url_groups_count_people, params=params_count_group)
                 group_count_people_list = group_file.json()['response']['count']
-                count_people_in_group.append(group_count_people_list)
+                # count_people_in_group.append(group_count_people_list)
                 print(f'\tvk.com/public{id_group}, с количеством пользователей {group_count_people_list}')
+                return group_count_people_list
             except:
                 print(group_file.json())
                 print(f'\t проблемы с vk.com/public{id_group} ')
 
     def id_friends(self):
         # № 1 id друзей User
-
         friends_file = requests.get(url_friends, params=self.params_id_friends)
         id_list = friends_file.json()['response']['items']
+        self.groups_user() #узнаем группы основного пользователся
         return id_list
 
-    def groups_friends(self):
-        # № 2 итерация по списку c id пользователей, с возвращением групп каждого пользователся
 
+    def groups_friends(self):
+        # № 2 итерация по списку c id пользователей, с возвращением находиться ли подписчик в группе или нет
         id_list = list(self.id_friends())
-        for id_user in id_list:
-            self.friend = f'user{id_user}'
-            self.friend = User(f'{id_user}')
-            self.friend.groups_user()
+        for group in self.groups_user():
+            scroe_frinds_no_group = 0
+            print(scroe_frinds_no_group)
+            self.id_group = group[0]
+
+            for id_user in id_list:
+                time.sleep(1)
+                params_group = {'v': 5.61,
+                                'access_token': token,
+                                'group_id': self.id_group,
+                                'user_id': id_user, }
+                res = requests.get(url_not_user_in_group, params=params_group)
+                if [0] == list((res.json()).values()): #если пользователя нет в группе основного пользователя
+                    scroe_frinds_no_group += 1
+                    print(len(id_list),'/',scroe_frinds_no_group)
+                    print('Search')
+                    if len(id_list) == scroe_frinds_no_group:
+                        print(f'В vk.com/public{self.id_group} нет никого из друзей ')
+                        self.user_alone_in_group()
+                else:
+                    print(list((res.json()).values()))
+                    break # прекращение интерации если хотя бы 1 человек из друзей есть в группе
+
+    def user_alone_in_group(self):
+        # Формирвоание json с группами, где только основной пользователь
+        group_file = requests.get(url_gloups, params=self.params_group)
+        items = group_file.json()['response']['items']
+
+        for el in items:
+            if el['id'] == self.id_group:
+                count_people = self.count_people_in_group(self.id_group) # сохр. кол-во пользователей группы
+                data_group = {"name": el['name'], "gid": el['id'], 'members_count': count_people}
+                with open('group.json', 'a', encoding='utf-8') as file:
+                    json.dump(data_group, file, ensure_ascii=False, indent=2)
+
 
 user1 = User('171691064')
 user1.groups_friends()
-print(f'СТОЛЬКО челов ЕБАТЬ {account_succses}')
+
+
+
 
 
--- test.py
+++ test.py
@@ -1,24 +1,31 @@
-# import requests
-# token = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
-# url_gloups = 'https://api.vk.com/method/groups.get'
+import json
+import requests
+token = "958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008"
+url_gloups = 'https://api.vk.com/method/groups.get'
 # url_friends = 'https://api.vk.com/method/friends.get'
 # url_groups_count_people = 'https://api.vk.com/method/groups.getMembers'
 #
-# # возвращает группы человека
-# list_group = []
-# params_group = {'v': 5.61,
-#                        'access_token': token,
-#                        'user_id': 171691064,
-#                        'extended': 1, }
-# group_file = requests.get(url_gloups, params=params_group)
-# # print(group_file.json())
-# items = group_file.json()['response']['items']
-# # print(items)
-# for el in items:
-#     personal_group = el['id'], el['name']
-#     list_group.append(list(personal_group))
-# print(len(list_group))
-# print(list_group)
+# возвращает группы человека
+list_group = []
+params_group = {'v': 5.61,
+                       'access_token': token,
+                       'user_id': 171691064,
+                       'extended': 1, }
+group_file = requests.get(url_gloups, params=params_group)
+
+items = group_file.json()['response']['items']
+
+for el in items:
+
+    if el['id'] == 8564:
+        count_people = self.count_people_in_group(id_group)
+        personal_group = el['id'], el['name']
+        data_group = {"name": el['name'], "gid": el['id'], 'members_count': count_people}
+        with open('group.json', 'a', encoding='utf-8') as file:
+
+            json.dump(data_group, file, ensure_ascii=False, indent=2 )
+
+
 
 
 # возвращает количество человек в сообществе
@@ -38,28 +45,51 @@
 
 
 
-
-
-
-
+# # есть ли пользователь в группе.
+# url = "https://api.vk.com/method/groups.isMember"
+# params_group = {'v': 5.61,
+#                        'access_token': token,
+#                        'group_id': 8564,
+#                        'user_id': 94762191, }
+# # 171691064
 #
+# res = requests.get(url, params=params_group)
+#
+# print(list((res.json()).values()) == 0)
+
+
+
+
+
 # a = {1: 'D', 2: 'B', 5: 'A', 3: 'B', 4: 'E'}
 # print(sorted({1: 'D', 2: 'B', 5: 'A', 3: 'B', 4: 'E'}))
 # print(sorted("This is a test string from Andrew".split(), key=str.lower))
 
-class Student:
-        def __init__(self, name, grade, age):
-            self.name = name
-            self.grade = grade
-            self.age = age
-        def __repr__(self):
-            return repr((self.name, self.grade, self.age))
-        def weighted_grade(self):
-            return 'CBA'.index(self.grade) / self.age
+# class Student:
+#         def __init__(self, name, grade, age):
+#             self.name = name
+#             self.grade = grade
+#             self.age = age
+#         def __repr__(self):
+#             return repr((self.name, self.grade, self.age))
+#         def weighted_grade(self):
+#             return 'CBA'.index(self.grade) / self.age
+#
+# student_objects = [
+#         Student('john', 'A', 15),
+#         Student('jane', 'B', 12),
+#         Student('dave', 'B', 10),
+#     ]
+# print(sorted(student_objects, key=lambda student: student.age))
 
-student_objects = [
-        Student('john', 'A', 15),
-        Student('jane', 'B', 12),
-        Student('dave', 'B', 10),
-    ]
-print(sorted(student_objects, key=lambda student: student.age))
+
+
+# s = 'ab'
+# t = (20,10 , 30)
+#
+# print(sorted((s,t), key=len))
+#
+#
+# import os
+#
+# print(os.getcwd())
